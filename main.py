68d53536c406f5b3411bba53f1f7ef915716bf95
Kheraa <BMC212275902@student.bmet.ac.uk>
2022-09-10 23:41:16 +0100

    Formatted structure

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ print("""Topic List:
 1. Variables
 """)
 number = input("Enter a topic using the numbers: ")
+print("-------------------------------------\n")
+
 if number == "1": 
-    print("Variables")
+    print("Variables:")
+    print("-----------")
     print("Variables are containers for storing data values. They have data types aswell as names which label them throughout the code and can be changed.")
     print("""Examples:
     1. name = \"Khera\"
