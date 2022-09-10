ec24bf71122110621974edd25dbda63527cdc9d7
Kheraa <BMC212275902@student.bmet.ac.uk>
2022-09-10 23:37:06 +0100

    Added examples for variables

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ number = input("Enter a topic using the numbers: ")
 if number == "1": 
     print("Variables")
     print("Variables are containers for storing data values. They have data types aswell as names which label them throughout the code and can be changed.")
+    print("""Examples:
+    1. name = \"Khera\"
+    2. age = 17
+    3. girl = True""")
