c8edaa856cd3a057042b21f898c4f5c263140185
Kheraa <BMC212275902@student.bmet.ac.uk>
2022-09-10 23:29:48 +0100

    Added information for varibles section

--- main.py
+++ main.py
@@ -4,3 +4,4 @@ print("""Topic List:
 number = input("Enter a topic using the numbers: ")
 if number == "1": 
     print("Variables")
+    print("Variables are containers for storing data values. They have data types aswell as names which label them throughout the code and can be changed.")
